63574071f77469f03d5d78e1832a1df6c24e558d
IndraniParbat <111188316+IndraniParbat@users.noreply.github.com>
2025-04-03 12:45:19 +0530

    Update indrani.py
    Comments added

--- indrani.py
+++ indrani.py
@@ -1,8 +1,9 @@
-n=int(input("give an input:"))
-def calculate_table():
+# AF to take input and print a table of the input till 100
+n=int(input("give an input:")) #input of a number
+def calculate_table(): #func defined
     x=0
     while x<=100:
       print(f"{n}*{x}=",n*x)
       x+=1
 
-calculate_table()
+calculate_table() #call the func
